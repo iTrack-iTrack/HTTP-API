366fd8e2ce30388f7ddc487535e7f31f2aeca6fa
Kevin J. Schurman <541863@student.saxion.nl>
2025-05-14 17:07:58 +0200

    Made the template code for the API.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log("Hello, world!");
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { createServer } from "http";
+import { open } from "sqlite";
+import sqlite3 from "sqlite3";
+
+dotenv.config();
+
+let server = async () => {
+	let app: Express = express();
+	app.use(cors({ origin: true, credentials: true }));
+
+	let db = await open({filename: String(process.env.DB_PATH), driver: sqlite3.Database});
+	let queryDatabase = async (res: Response, query: string, params: any[] = []) => {
+		res.send(await db.all(query, params));
+	};
+
+	app.get("/:id", (req: Request, res: Response) => {
+		queryDatabase(res,
+			      "SELECT * FROM sensor_readings WHERE user_id = ?",
+			      [req.params.id])
+	});
+
+	let port: Number = Number(process.env.PORT) || 8080;
+	let http = createServer(app);
+	http.listen(port, () => {
+		    console.log(`[server]: listening on port http://localhost:${port}.`)
+	});
+};
+server();
