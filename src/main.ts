926362e914c83c189fe0246ff2cb9bbca8e732e6
Kevin J. Schurman <541863@student.saxion.nl>
2025-06-06 12:13:30 +0200

    Reformatted code and added more detail to querying the database.

--- src/main.ts
+++ src/main.ts
@@ -14,14 +14,18 @@ let server = async () => {
 	app.use("/picture", express.static(String(process.env.PFP_PATH)));
 
 	let db = await open({filename: String(process.env.DB_PATH), driver: sqlite3.Database});
-	let queryDatabase = async (res: Response, query: string, params: any[] = []) => {
-		res.send(await db.all(query, params));
-	};
 
-	app.get("/:id", (req: Request, res: Response) => {
-		queryDatabase(res,
-			      "SELECT * FROM sensor_readings WHERE user_id = ?",
-			      [req.params.id])
+	let queryDatabase = async (query: string, params: any[] = []) => await db.all(query, params);
+
+	app.get("/:user_id", async (req: Request, res: Response) => {
+		let result = await queryDatabase(
+			"SELECT s.*"
+			"FROM users u, sensor_data s"
+			"WHERE u.user_id = ?"
+			"AND u.user_id = s.user_id",
+			[req.params.user_id]
+		);
+		res.send(result);
 	});
 
 	let port: Number = Number(process.env.PORT) || 8082;
